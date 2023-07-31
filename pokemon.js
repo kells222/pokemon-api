4ea483e226a274c666210cbd64d2a170684179f1
Kelly Buckley <princesskells@Kellys-MacBook-Air.local>
2023-07-31 16:00:12 -0700

    end of practice

--- pokemon.js
+++ pokemon.js
@@ -1,1 +1,20 @@
+
+const axios = require("axios")
 const BASEURL = "https://pokeapi.co/api/v2"
+
+async function getPokes (URL){
+try {
+   const result= await axios.get(`${URL}/pokemon`);
+   const {data} = result 
+   console.log(data)
+//    console.log(result)
+    // .then(res=>{
+    //     const {data}=res
+    //     console.log(data)
+    // })
+
+} catch (error) {
+    
+}}
+
+getPokes(BASEURL)
